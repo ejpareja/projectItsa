2d10319693e54b818ee7ab3ce78fdb334cdb04f2
FredysMuriel <75293790+FredysMuriel@users.noreply.github.com>
2020-12-02 02:19:44 -0500

    export y menu
    El export ya le metí la función de exportar a excel y el menu editado como pediste, con el error que te dije en wpp de qu emandaba DAM

--- Export.py
+++ Export.py
@@ -1,8 +1,12 @@
+from tkinter import *
+from tkinter import StringVar
+from tkinter import messagebox
+from tkinter import ttk
 import openpyxl
 import Queries
 from datetime import date
 
-def get_users():
+def export_excel():
     wb = openpyxl.Workbook()
     sheet = wb.active
     sheet.title = "reporte"
@@ -13,33 +17,40 @@ def get_users():
         name = i[1]
         role = i[2]
         sheet.append((ide,name, role))
+    
     now = date.today()
+   
     wb.save("reporteusuario"+str(now)+".xlsx")
 
-def get_invoices()
-    wb = openpyxl.Workbook()
-    sheet = wb.active
-    sheet.title = "reporte"
-    data = Queries.get_invoices()
-    sheet.append(('id', 'nombre', 'rol'))
-    for i in data:
-        ide = i[0]
-        name = i[1]
-        role = i[2]
-        sheet.append((ide,name, role))
-    now = date.today()
-    wb.save("reporteproductos"+str(now)+".xlsx")
+export_excel()
 
-def get_invoices()
-    wb = openpyxl.Workbook()
-    sheet = wb.active
-    sheet.title = "reporte"
-    data = Queries.get_invoices()
-    sheet.append(('id', 'nombre', 'rol'))
-    for i in data:
-        ide = i[0]
-        name = i[1]
-        role = i[2]
-        sheet.append((ide,name, role))
-    now = date.today()
-    wb.save("reporteproductos"+str(now)+".xlsx")
+def back():
+	root.destroy()
+	import menu.py
+	menu.meMain(data)
+
+font = ('Bahnschrift Light', 12) 
+root = Tk()
+root.geometry("760x400+300+100")
+root.resizable(False,False)
+root.title("Registrar Usuario")
+root.iconbitmap('presupuesto.ico')
+root.config(bg="#222f3e")
+#Treeview
+tree = ttk.Treeview(root)
+tree["columns"] = ("Identificación", "Nombre", "Rol")
+tree.column("#0", width=30, minwidth=270, stretch=NO)
+tree.column("Identificación", width=130, minwidth=270, stretch=NO)
+tree.column("Nombre", width=200, minwidth=270, stretch=NO)
+tree.column("Rol", width=100, minwidth=270, stretch=NO)
+
+tree.heading("#0",text="",anchor=W)
+tree.heading("Identificación", text="Identificación",anchor=W)
+tree.heading("Nombre", text="Nombre",anchor=W)
+tree.heading("Rol", text="Rol",anchor=W)
+tree.place(x=150,y=50)
+
+#Button
+Button(root, text="Exportar", font=font, bg="#c8d6e5", fg="black", command=export_excel).place(x=540, y=300)
+Button(root, text="Regresar", font=font, bg="#c8d6e5", fg="black", command=back).place(x=150, y=300)
+root.mainloop()
--- menu.py
+++ menu.py
@@ -1,36 +1,43 @@
 from tkinter import *
 from tkinter import StringVar
 from tkinter import messagebox
-from datetime import date
+global name 
+global ide
+global role
 
+def meMain(data):
+	for i in data:
+		ide = i[0]
+		name = i[1]
+		role = i[2]
+	print(ide, name, role)
+	loadInt(ide, name, role)
 
-def intRegister(root):
+def intRegister():
 	root.destroy()
 	import register.py
 def intFacturation():
 	root.destroy()
 	import facturation.py
 
-def load_interface(name, ide, role):
-	date_now = date.today()
+def loadInt(name, ide, role):
 	font1 = ('Bahnschrift Light', 14) 
 	font = ('Bahnschrift Light', 12)
 	root = Tk()
-	root.geometry("330x320+450+100")
+	root.geometry("500x400+350+100")
 	root.resizable(False,False)
 	root.title("Menú")
 	root.iconbitmap('presupuesto.ico')
 	root.config(bg="#222f3e")
 
-	
-	
-	Label(root, text="Menú", font=font1, bg="#c8d6e5", fg="black").place(x=0, y=0, width="330", height="50")
-	#RegisterWindow
-	Label(root, text=name, font=font, bg="#222f3e", fg="white").place(x=30, y=80)
-	Label(root, text="1. Registrar Usuario:", font=font, bg="#222f3e", fg="white").place(x=30, y=100)
-	Button(root, text="Click Aquí", font=font, command=intRegister(root),bg="#c8d6e5", fg="black").place(x=210, y=75)
-	#FacturationWindow
-	Label(root, text="2. Facturar:", font=font,bg="#222f3e", fg="white").place(x=30, y=130)
-	Button(root, text="Click Aquí", font=font, command=intFacturation(root),bg="#c8d6e5", fg="black").place(x=210, y=125)
-	print(name, ide, role)
+	Label(root, text="Menú", font=font1, bg="#c8d6e5", fg="black").place(x=0, y=0, width="500", height="50")
+	#Data
+	Label(root, text=ide, font=font, bg="#222f3e", fg="white").place(x=30, y=80)
+	Label(root, text=name, font=font, bg="#222f3e", fg="white").place(x=130, y=80)
+	Label(root, text=role, font=font, bg="#222f3e", fg="white").place(x=250, y=80)
+	#Buttons
+	Button(root, text="Registrar", font=font1, command=intRegister, bg="#c8d6e5", fg="black").place(x=30, y=150, width="120", height="50")
+	Button(root, text="Facturar", font=font1, command=intFacturation, bg="#c8d6e5", fg="black").place(x=190, y=150, width="120", height="50")
+	Button(root, text="Reportes", font=font1, bg="#c8d6e5", fg="black").place(x=350, y=150, width="120", height="50")
+	Button(root, text="Salir", font=font, bg="#c8d6e5", fg="black").place(x=420, y=80)
 	root.mainloop()
